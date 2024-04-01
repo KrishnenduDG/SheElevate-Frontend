ddff7cb2e3220cdf21fd38b5e8dc603e3b30df01
KrishnenduDG <krishnendu.kdg@gmail.com>
2024-04-01 17:21:36 +0530

    Just a change to test deployment routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App-Home-Page</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/contact-us" element={<h1>Contact Us</h1>} />
+    </Routes>
+  );
 };
 
 export default App;
