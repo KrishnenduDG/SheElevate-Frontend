fa89dc8d9c9923fcc7ea3edd2bf0ea631f786149
shrestha <shresthachakrabortykgp@gmail.com>
2024-12-13 00:03:06 +0530

    HomePage design done

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -37,7 +37,7 @@ const HomePage = () => {
   return (
     <>
       <div className="bg-gradient-to-r from-violet-950 to-black relative">
-        <div className="flex py-6">
+        <div className="flex py-9">
           <h1 className="text-white font-serif text-3xl mt-12 z-0 leading leading-relaxed text-center capitalize py-5 text-wrap">
             Each time a woman stands up for herself,
             <br /> without knowing it possibly,
@@ -88,16 +88,22 @@ const HomePage = () => {
       </div>
 
       {/* About Us Section */}
-      <div id="about" className="py-20 bg-gray-100">
+      <div id="about" className="py-20">
         <div className="container mx-auto px-6 md:px-12 text-center">
-          <h2 className="text-4xl font-bold text-violet-900 mb-6">About Us</h2>
-          <p className="text-lg text-gray-700 leading-relaxed">
-            She Elevate is a platform dedicated to empowering skilled women in
+          <h2 className="text-5xl font-bold text-violet-900 mb-6">About Us</h2>
+          <p className="text-2xl text-gray-700 leading-relaxed">
+            SheElevate is a platform dedicated to empowering skilled women in
             rural areas by connecting them with local business owners and shop
-            keepers. Through this initiative, we aim to bridge the gap between
-            talent and opportunity, fostering creativity and economic growth. By
+            keepers. Women who are proficient or skilled in any work, often lack
+            the support and a platform where their work is appreciated and
+            eventually the skill is either shut in a vault or just wasted. By
             enabling women to showcase their crafts and skills, we contribute to
-            building a sustainable and inclusive society.
+            building a sustainable and inclusive society. SheElevate empowers
+            women financially and even acts as a centre of empowerment which not
+            only benefits them but also common people who can easily access
+            items from them. Through this initiative, we aim to bridge the gap
+            between talent and opportunity, fostering creativity and economic
+            growth.
           </p>
         </div>
       </div>
@@ -107,12 +113,7 @@ const HomePage = () => {
         id="contact"
         className="py-20  pt-0 bg-gradient-to-r from-violet-950 to-black relative text-white"
       >
-        <svg
-          viewBox="0 0 1440 320"
-          //version="1.1"
-          //id="svg1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
           <path
             fill="#ffffff"
             d="M 0 0 L 0 160 L 30 176 C 60 192 120 223.99922 180 234.69922 C 240 244.99922 300 235.00078 360 213.30078 C 420 192.00078 480 160 540 176 C 600 192 660 256.00078 720 245.30078 C 780 235.00078 840 148.99922 900 106.69922 C 960 63.999219 1020 64.000781 1080 85.300781 C 1140 107.00078 1200 149.00078 1260 181.30078 C 1320 213.00078 1380 235.00078 1410 245.30078 L 1440 256 L 1440 0 L 0 0 z "
